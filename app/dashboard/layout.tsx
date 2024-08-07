18d8fd0238a4aefe194705d346c3ac33dcc9358a
shishui010 <552033445@qq.com>
2024-08-07 18:02:42 +0800

    登录、授权

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
  
+// 局部渲染  nextconfig.js 配置 experimental
+export const experimental_ppr = true;
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
